c91b16346b4b50a25802d465716124d4a2ddfcc2
Dowes Mbella <dowesw@gmail.com>
2014-06-03 10:07:22 +0100

    MAJ

--- includes/lang/fr/tabella.php
+++ includes/lang/fr/tabella.php
@@ -4,13 +4,13 @@ switch ($messaggio) {
 
 case "situazione alle":  			$messaggio = "situation à"; break;
 case "del":  					$messaggio = "de"; break;
-case "Tabella prenotazioni del":  		$messaggio = "table de réservations de"; break;
+case "Tabella prenotazioni del":  		$messaggio = "Tableau de réservations de"; break;
 case "ERRORE":  				$messaggio = "ERREUR"; break;
-case "Torna al menù principale":  		$messaggio = "retour au menu principal"; break;
-case "Visualizza la tabella per la stampa":  	$messaggio = "vue la table pour imprimer"; break;
-case "Visualizza la tabella con i giorni":  	$messaggio = "vue la table avec des jours"; break;
+case "Torna al menù principale":  		$messaggio = "Retour au menu principal"; break;
+case "Visualizza la tabella per la stampa":  	$messaggio = "Vue imprimable de la table"; break;
+case "Visualizza la tabella con i giorni":  	$messaggio = "Vue la table avec des jours"; break;
 case "documento di tipo":  			$messaggio = "type de document"; break;
-case "visualizza":  				$messaggio = "vue"; break;
+case "visualizza":  				$messaggio = "Vue"; break;
 case "Prenotazione non confermata":  		$messaggio = "réservation non confirmée"; break;
 case "Prenotazione confermata, caparra non pagata":	$messaggio = "réservation confirmée, déposent non payé"; break;
 case "Caparra pagata":  			$messaggio = "dépôt payé"; break;
